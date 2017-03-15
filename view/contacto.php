fbb05b08c9170edff1e62c658f3efb173ec532cc
Unknown <danielfernandez31295@hotmail.es>
2017-03-15 16:40:46 +0100

    Pagina contacto añadida

---
+++ view/contacto.php
@@ -0,0 +1,25 @@
+<div class="container-fluid ">
+    <div class="articulo contenido"> 
+            <div class="form-area">  
+                <form>
+                    <h3>Formulario de contacto</h3>
+                    <br>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="name" name="nombre" placeholder="Nombre" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="email" name="email" placeholder="Email" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="subject" name="asunto" placeholder="Asunto" required>
+                    </div>
+                    <div class="form-group">
+                        <textarea class="form-control" type="textarea" id="mensaje" placeholder="Mensaje" maxlength="140" rows="7"></textarea>               
+                    </div>
+
+                    <button type="button" id="submit" name="submit" class="btn btn-primary pull-right">Enviar</button>
+                </form>
+            </div>
+        
+    </div>
+</div>
