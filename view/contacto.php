8e9e3980b59fae912b346548e1147a25c7503710
Unknown <cristinaroige75@gmail.com>
2017-04-01 20:41:46 +0200

    Canvis vistes footer, formulari de contacte, navar i manual d'usuari repassat

--- view/contacto.php
+++ view/contacto.php
@@ -2,19 +2,19 @@
     <div class="articulo contenido"> 
             <div class="form-area">  
                 <form>
-                    <h3>Formulario de contacto</h3>
+                    <h3>Formulari de contacte</h3>
                     <br>
                     <div class="form-group">
-                        <input type="text" class="form-control" id="name" name="nombre" placeholder="Nombre" required>
+                        <input type="text" class="form-control" id="name" name="nombre" placeholder="Nom" required>
                     </div>
                     <div class="form-group">
                         <input type="text" class="form-control" id="email" name="email" placeholder="Email" required>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" id="subject" name="asunto" placeholder="Asunto" required>
+                        <input type="text" class="form-control" id="subject" name="asunto" placeholder="Assumpte" required>
                     </div>
                     <div class="form-group">
-                        <textarea class="form-control" type="textarea" id="mensaje" placeholder="Mensaje" maxlength="140" rows="7"></textarea>               
+                        <textarea class="form-control" type="textarea" id="mensaje" placeholder="Missatge" maxlength="140" rows="7"></textarea>               
                     </div>
 
                     <button type="button" id="submit" name="submit" class="btn btn-primary pull-right">Enviar</button>
